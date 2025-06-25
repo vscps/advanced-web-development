e2a5365c7ce64467721805df3d6a92a3c3ff3197
Daniel <daniel.seebacher17@gmail.com>
2025-06-25 14:41:29 +0200

    Solved challenge 1

---
+++ week1/web-challenges/challenge1.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    username: "David",
+    status: "online",
+    lastActivity: 10,
+  },
+  {
+    username: "Lucy",
+    status: "offline",
+    lastActivity: 22,
+  },
+  {
+    username: "Bob",
+    status: "online",
+    lastActivity: 104,
+  },
+];
+
+function userStatus(data) {
+  const away = data
+    .filter((entry) => entry.status === "online" && entry.lastActivity > 10)
+    .map((entry) => entry.username);
+  const online = data
+    .filter((entry) => entry.status === "online" && entry.lastActivity <= 10)
+    .map((entry) => entry.username);
+  const offline = data
+    .filter((entry) => entry.status === "offline")
+    .map((entry) => entry.username);
+
+  return {
+    online,
+    away,
+    offline,
+  };
+}
+
+console.log(userStatus(data));
