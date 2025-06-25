2fb5e4beabdf464a81abd8ccf675b8d3af01fe95
Daniel <daniel.seebacher17@gmail.com>
2025-06-25 14:18:58 +0200

    Initialized project 1

---
+++ Challenge1.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    username: "David",
+    status: "online",
+    lastActivity: 10,
+  },
+  {
+    username: "Lucy",
+    status: "offline",
+    lastActivity: 22,
+  },
+  {
+    username: "Bob",
+    status: "online",
+    lastActivity: 104,
+  },
+];
